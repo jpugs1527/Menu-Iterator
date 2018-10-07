0cdb0ba63100c275b435e67251040833cff4bbef
Jordan Puglisi <jpugs1527@yahoo.com>
2018-10-07 15:24:55 -0400

    Adding Iterators

--- src/menuIterator/Menu.java
+++ src/menuIterator/Menu.java
@@ -3,13 +3,13 @@ package menuIterator;
 /**
  * @author jordanpuglisi
  */
-
 import java.util.*;
 import org.apache.commons.lang3.ArrayUtils;
 
 public class Menu {
+
     private MenuItem first_item;
-    
+
     int index = 0;
     MenuItem[] arr; //Initialize an array of type MenuItem to hold MenuItem objs
     public static final int APPETIZER = 1;
@@ -17,19 +17,70 @@ public class Menu {
     public static final int DESSERT = 3;
     public static final boolean HEARTHEALTHY = true;
     public static final boolean NOTHEARTHEALTHY = false;
-    
+
     public Menu() {
         arr = new MenuItem[5];
     }
-    
+
+    //Maybe refactor to use an ArrayUtils method
     public void append(MenuItem item) {
         arr[index] = item;
         index++;
     }
-    
-    public void delete(MenuItem item) {
+
+    public void remove(MenuItem item) {
         arr = ArrayUtils.removeElement(arr, item);
     }
-    
-    
+
+    private class AllItemsIterator implements MenuIterator {
+
+        int index = 0;
+        int end;
+        MenuItem item;
+
+        public AllItemsIterator(int end) {
+            this.end = arr.length;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index <= end;
+        }
+
+        @Override
+        public MenuItem next() {
+            if (index < 0) {
+                throw new NoSuchElementException();
+            } else {
+                MenuItem nextItem = arr[index];
+                index++;
+                return nextItem;
+            }
+        }
+    }
+
+    private class ItemIterator implements MenuIterator {
+
+        int index = 0;
+        int end;
+        int itemType;
+
+        public ItemIterator(int end, int itemType) {
+            this.end = arr.length;
+            this.itemType = itemType;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index <= end;
+        }
+        
+        @Override
+        public boolean next() {
+            MenuItem item = arr[index];
+            if ()
+        }
+
+    }
+
 }
